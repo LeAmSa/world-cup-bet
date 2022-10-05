cadddd4111a709542f1a66460b3a41d2a155799f
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-05 10:19:26 -0300

    update dashboard page

--- web/src/pages/Dashboard/index.jsx
+++ web/src/pages/Dashboard/index.jsx
@@ -1,8 +1,9 @@
-import Icon from "../../components/Icon";
+import MatchCard from "~/components/MatchCard";
+import Icon from "~/components/Icon";
 
 function Dashboard() {
   return (
-    <div className="">
+    <>
       <header className="bg-red-500 text-white p-4">
         <div className="container max-w-3xl flex justify-between">
           <img className="w-28 md:w-40" src="/logo-bg-red.svg" />
@@ -12,17 +13,36 @@ function Dashboard() {
         </div>
       </header>
 
-      <main>
-        <section id="header" className="bg-red-500 text-white p-4">
-          <div className="container max-w-3xl space-y-2">
+      <main className="space-y-6">
+        <section id="header" className="bg-red-500 text-white">
+          <div className="container max-w-3xl space-y-2 p-4">
             <span>Olá Leandro!</span>
             <h3 className="text-2xl font-bold">Qual é o seu palpite?</h3>
           </div>
         </section>
 
-        <section id="content"></section>
+        <section id="content" className="container max-w-3xl p-4 space-y-4">
+          <div className="flex p-4 space-x-4 justify-center items-center">
+            <Icon name="arrowLeft" className="text-red-500" />
+            <span className="font-bold text-base">24 de novembro</span>
+            <Icon name="arrowRight" className="text-red-500" />
+          </div>
+
+          <div className="space-y-4">
+            <MatchCard
+              home={{ slug: "sui" }}
+              away={{ slug: "cam" }}
+              match={{ time: "7:00" }}
+            />
+            <MatchCard
+              home={{ slug: "por" }}
+              away={{ slug: "gan" }}
+              match={{ time: "7:00" }}
+            />
+          </div>
+        </section>
       </main>
-    </div>
+    </>
   );
 }
 
