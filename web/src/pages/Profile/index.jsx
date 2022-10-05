f7e62ff8d81f176b1b099a1488d60d3a7972b729
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-05 10:18:35 -0300

    create Profile page

---
+++ web/src/pages/Profile/index.jsx
@@ -0,0 +1,50 @@
+import MatchCard from "~/components/MatchCard";
+import Icon from "~/components/Icon";
+
+function Profile() {
+  return (
+    <>
+      <header className="bg-red-500 text-white p-4">
+        <div className="container max-w-3xl flex justify-between">
+          <img className="w-28 md:w-40" src="/logo-bg-red.svg" />
+        </div>
+      </header>
+
+      <main className="space-y-6">
+        <section id="header" className="bg-red-500 text-white">
+          <div className="container max-w-3xl space-y-2 p-4">
+            <a href="/dashboard">
+              <Icon name="back" />
+            </a>
+            <h3 className="text-2xl font-bold">Leandro</h3>
+          </div>
+        </section>
+
+        <section id="content" className="container max-w-3xl p-4 space-y-4">
+          <h2 className="text-xl text-red-500 font-bold">Seus palpites</h2>
+
+          <div className="flex p-4 space-x-4 justify-center items-center">
+            <Icon name="arrowLeft" className="text-red-500" />
+            <span className="font-bold text-base">24 de novembro</span>
+            <Icon name="arrowRight" className="text-red-500" />
+          </div>
+
+          <div className="space-y-4">
+            <MatchCard
+              home={{ slug: "sui" }}
+              away={{ slug: "cam" }}
+              match={{ time: "7:00" }}
+            />
+            <MatchCard
+              home={{ slug: "por" }}
+              away={{ slug: "gan" }}
+              match={{ time: "7:00" }}
+            />
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Profile;
