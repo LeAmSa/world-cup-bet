bbfe352d15d13aac1ef12a1312242d63d0176bcf
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-06 22:07:43 -0300

    add form validation props

--- web/src/components/Input/index.jsx
+++ web/src/components/Input/index.jsx
@@ -1,16 +1,32 @@
-function Input({ name, type, placeholder, label }) {
+function Input({
+  name,
+  type,
+  placeholder,
+  label,
+  value,
+  onChange,
+  onBlur,
+  error,
+}) {
   return (
     <div className="flex flex-col">
       <label className="text-sm font-bold text-gray-500 mb-2" htmlFor={name}>
         {label}
       </label>
       <input
-        className="p-3 border border-gray-700 rounded-xl focus:outline focus:outline-1"
+        className={`p-3 border border-gray-700 rounded-xl focus:outline focus:outline-1 ${
+          error && "border-red-300"
+        }`}
         name={name}
         id={name}
         type={type}
         placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        error={error}
       />
+      <span className="p-2 text-sm text-red-300">{error}</span>
     </div>
   );
 }
