5e577bb1beaa818e3da9edb4e04b34cc6ad8fe86
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-06 22:07:24 -0300

    add form validation

--- web/src/pages/SignUp/index.jsx
+++ web/src/pages/SignUp/index.jsx
@@ -1,7 +1,43 @@
 import Icon from "~/components/Icon";
 import Input from "~/components/Input";
+import axios from "axios";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+//Validando os dados
+const validationSchema = yup.object().shape({
+  name: yup.string().required("Preencha seu nome"),
+  username: yup.string().required("Preencha seu username"),
+  email: yup
+    .string()
+    .email("Digite um e-mail válido")
+    .required("Preencha seu email"),
+  password: yup.string().required("Preencha sua senha"),
+});
 
 function SignUp() {
+  const formik = useFormik({
+    onSubmit: async (values) => {
+      const res = await axios({
+        method: "post",
+        baseURL: "http://localhost:3000",
+        url: "/users",
+        data: values,
+      });
+
+      console.log(res.data);
+    },
+    initialValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+    validationSchema,
+  });
+
+  console.log(formik.errors);
+
   return (
     <div>
       <header className="p-4 border-b border-red-300">
@@ -18,12 +54,16 @@ function SignUp() {
           <h2 className="text-xl font-bold">Crie sua conta</h2>
         </div>
 
-        <form className=" p-4 space-y-6" action="">
+        <form className=" p-4 space-y-6" onSubmit={formik.handleSubmit}>
           <Input
             name="name"
             type="text"
             placeholder="Digite seu nome"
             label="Seu nome"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.name && formik.errors.name}
           />
 
           <Input
@@ -31,12 +71,20 @@ function SignUp() {
             type="text"
             placeholder="Digite um nome de usuário"
             label="Seu nome de usuário"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.username && formik.errors.username}
           />
           <Input
             name="email"
             type="text"
             placeholder="Digite seu email"
             label="Seu e-mail"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.email && formik.errors.email}
           />
 
           <Input
@@ -44,14 +92,19 @@ function SignUp() {
             type="password"
             placeholder="Digite sua senha"
             label="Sua senha"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.password && formik.errors.password}
           />
 
-          <a
-            href="/dashboard"
-            className="block w-full text-white bg-red-500 text-lg md:text-xl px-6 py-3 rounded-xl text-center"
+          <button
+            type="submit"
+            className="block w-full text-white bg-red-500 text-lg md:text-xl px-6 py-3 rounded-xl text-center disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={!formik.isValid}
           >
             Criar minha conta
-          </a>
+          </button>
         </form>
       </main>
     </div>
